7c3967438f75596d853ead7de7738a83c5ffc77c
DanielBilotto <dbilotto01@yahoo.com>
2021-10-06 17:34:58 -0400

    lab 5

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,10 @@
+var x = document.getElementsByClassName("title");
+    for (var i = 0; i < x.length; i++)
+    {
+        x[i].innerText = "New Text!";
+    }
+    document.getElementById("bossy").style.fontWeight = 'normal';
+document.getElementById("blue-li").style.color = 'blue';
+document.getElementById("cream-li").style.backgroundColor = 'Moccasin';
+document.body.style.backgroundColor = 'grey';
+document.getElementById("upper-li").style.textTransform = 'uppercase';

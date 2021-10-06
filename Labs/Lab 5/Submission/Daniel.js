8dc40209bde441553e69f50c9149e6871834c18c
DanielBilotto <dbilotto01@yahoo.com>
2021-10-06 18:34:25 -0400

    Update Daniel.js

--- Labs/Lab 5/Submission/Daniel.js
+++ Labs/Lab 5/Submission/Daniel.js
@@ -1,7 +1,7 @@
 var x = document.getElementsByClassName("title");
     for (var i = 0; i < x.length; i++)
     {
-        x[i].innerText = "New Text!";
+        x[i].innerText = "New Title!";
     }
     document.getElementById("bossy").style.fontWeight = 'normal';
 document.getElementById("blue-li").style.color = 'blue';

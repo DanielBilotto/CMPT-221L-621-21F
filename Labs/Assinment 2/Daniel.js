9863db52cc54d903c3a8cff000eb7dce440341af
DanielBilotto <dbilotto01@yahoo.com>
2021-11-03 08:50:58 -0400

    assignment 2

---
+++ Labs/Assinment 2/Daniel.js
@@ -0,0 +1,15 @@
+var x = document.getElementsByClassName("title");
+    for (var i = 0; i < x.length; i++)
+    {
+        x[i].innerText = "New Title!";
+    }
+document.getElementById("bossy").innerText = 'lessbossy';
+document.getElementById("blue-li").style.color = 'blue';
+document.getElementById("cream-li").style.backgroundColor = 'Moccasin';
+document.body.style.backgroundColor = 'grey';
+document.getElementById("upper-li").style.textTransform = 'uppercase';
+
+function addFriend()
+{
+    document.querySelector(".test").style.backgroundColor = "green";
+}

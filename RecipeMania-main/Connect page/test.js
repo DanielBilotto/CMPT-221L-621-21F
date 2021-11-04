7d225e372806981b90f9bba31876f37c98dc38b2
DanielBilotto <dbilotto01@yahoo.com>
2021-11-03 20:30:16 -0400

    assingnment 2

---
+++ RecipeMania-main/Connect page/test.js
@@ -0,0 +1,3 @@
+document.querySelector(".john").style.fontSize = "xx-large";
+document.querySelector(".jane").style.fontSize = "xx-large";
+document.querySelector(".josh").style.fontSize = "xx-large";

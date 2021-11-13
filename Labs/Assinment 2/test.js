62a57b44f395c0d3f016a1f4fa1da2f50bccb283
DanielBilotto <dbilotto01@yahoo.com>
2021-11-13 16:25:34 -0500

    still working out the kinks

--- Labs/Assinment 2/test.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".john").style.fontSize = "xx-large";
-document.querySelector(".jane").style.fontSize = "xx-large";
-document.querySelector(".josh").style.fontSize = "xx-large";

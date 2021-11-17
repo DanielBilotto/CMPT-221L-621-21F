21141c3a0755f7d9582932b900594fb83e45c87f
DanielBilotto <dbilotto01@yahoo.com>
2021-11-17 16:12:41 -0500

    lab 8

--- Labs/Lab 7/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-
-const port = 3000;
-
-mongoose.connect("mongodb+srv://Student:CorgisAreDope@cluster0.h6c8l.mongodb.net/test");

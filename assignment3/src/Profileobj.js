d1b8f506c06a182ed2ef966d626335d338923312
DanielBilotto <dbilotto01@yahoo.com>
2021-12-01 17:36:45 -0500

    all in

--- assignment3/src/Profileobj.js
+++ assignment3/src/Profileobj.js
@@ -6,6 +6,7 @@ function Profileobj(props)
         <div className="Profileobj">
             <img src={props.profile.imgURL} height = {150} width = {150}/>
             <h3>{props.profile.name}</h3>
+            <h4>{props.profile.user}</h4>
             <button>Add!</button>
             <hr/>   
         </div>
